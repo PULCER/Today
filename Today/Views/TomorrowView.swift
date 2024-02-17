ca99a5964d0c9a092c7886f7e0d95088923ecabc
PULCER <anthony@pulcer.net>
2024-02-17 15:34:14 -0800

    navigation working

--- Today/Views/TomorrowView.swift
+++ Today/Views/TomorrowView.swift
@@ -8,12 +8,127 @@ struct TomorrowView: View {
     @State private var newToDoText = ""
     @State private var showingAddToDo = false
     
+    private var todaysTasks: [ToDoListItem] {
+            let calendar = Calendar.current
+            let filteredItems = toDoListItems.filter { item in
+                calendar.isDate(item.timestamp, inSameDayAs: Date())
+            }
+            // Sort the filtered items here
+            return filteredItems.sorted { item1, item2 in
+                if item1.isCompleted && !item2.isCompleted {
+                    return false
+                } else if !item1.isCompleted && item2.isCompleted {
+                    return true
+                } else {
+                    return item1.timestamp < item2.timestamp
+                }
+            }
+        }
+    
     var body: some View {
-        VStack{
-            
+        VStack {
             Text("Tomorrow")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+            
+            List {
+                ForEach(todaysTasks) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.toDoListText)
+                        }
+                        Spacer()
+                        
+                        Button(action: {
+                            item.isCompleted.toggle()
+                        }) {
+                            Image(systemName: item.isCompleted ? "checkmark.circle.fill" : "circle")
+                        }
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            Spacer()
+            
+            HStack {
+                
+                Button(action: {
+                    navigationViewModel.currentScreen = .today
+                }) {
+                    Image(systemName: "chevron.backward")
+                }.padding()
+                
+                Button(action: {
+                    self.showingAddToDo = true
+                }) {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 48, height: 48)
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+                
+                Button(action: {
+                    navigationViewModel.currentScreen = .tomorrow
+                }) {
+                    Image(systemName: "chevron.forward")
+                }.padding()
+                    .opacity(0)
+            }
+            
+        }.sheet(isPresented: $showingAddToDo) {
+            
+            VStack {
+                TextField("Enter new task", text: $newToDoText)
+                
+                Spacer()
+                
+                HStack{
+                    
+                    Button("Discard") {
+                        newToDoText = ""
+                        self.showingAddToDo = false
+                    }
+                    .foregroundColor(.gray)
+                    .font(.title3)
+                    
+                    Spacer()
+                    
+                    Button("Save") {
+                        addItem()
+                        self.showingAddToDo = false
+                    }
+                    .foregroundColor(.gray)
+                    .font(.title3)
+                    
+                }.padding(.vertical)
+                
+            }
+            .padding()
+            .presentationDetents([.height(100)])
+        }
+        
+    }
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = ToDoListItem(timestamp: Date(), toDoListText: newToDoText, isCompleted: false)
+            modelContext.insert(newItem)
+            newToDoText = ""
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.forEach { offset in
+                let idToDelete = todaysTasks[offset].id
+                if let indexToDelete = toDoListItems.firstIndex(where: { $0.id == idToDelete }) {
+                    let itemToDelete = toDoListItems[indexToDelete]
+                    modelContext.delete(itemToDelete)
+                }
+            }
         }
     }
 }
+
+

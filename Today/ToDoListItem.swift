50fd13f5b6ff2a83968bf03efce705b3b9fa34d0
PULCER <anthony@pulcer.net>
2024-02-17 14:40:59 -0800

    model updated now the fun part.

--- Today/ToDoListItem.swift
+++ Today/ToDoListItem.swift
@@ -4,8 +4,12 @@ import SwiftData
 @Model
 final class ToDoListItem {
     var timestamp: Date
+    var toDoListText: String
+    var isCompleted: Bool
     
-    init(timestamp: Date) {
+    init(timestamp: Date, toDoListText: String, isCompleted: Bool = false) {
         self.timestamp = timestamp
+        self.toDoListText = toDoListText
+        self.isCompleted = isCompleted
     }
 }

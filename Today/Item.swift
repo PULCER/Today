b99ce20ca163ef9419cfbeb24592fc61a894429c
PULCER <anthony@pulcer.net>
2024-02-17 14:16:35 -0800

    Initial Commit

---
+++ Today/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Today
+//
+//  Created by Anthony Howell on 2/17/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
